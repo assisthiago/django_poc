040aa63ce55b1c940f2410feb0b54689a453b931
Thiago Assis <thiago.medassis@gmail.com>
2022-04-09 18:00:38 -0300

    Twilio

--- oftalmo/patient/admin.py
+++ oftalmo/patient/admin.py
@@ -1,5 +1,26 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+
+from twilio.rest import Client
 
 from .models import Patient
 
-admin.site.register(Patient)
+
+@admin.action(description='Mandar mensagem pelo Whatsapp')
+def send_whatsapp_message(modeladmin, request, queryset):
+    client = Client()
+    client.messages.create(
+        body='Olá, aqui é o oftalmo.',
+        from_='whatsapp:+14155238886',
+        to=f'whatsapp:+55{queryset[0].phone_number}'
+    )
+
+    messages.success(request, 'A mensagem pelo Whatsapp foi enviada com sucesso.')
+
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('cpf', 'name', 'phone_number', 'birthdate',)
+
+    actions = [send_whatsapp_message]
+
